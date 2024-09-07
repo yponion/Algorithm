6c8be5d31bb6a89a238ed149870cfa34a8ac527e
yponion <yp071704@naver.com>
2024-09-07 12:26:28 +0900

    ✨feat: 검문 / 유클리드 호제법
     - 나머지가 모두 같은 것들이니 값들의 차이에 대해 최대 공약수의 약수들을 구하면 되었다
     - for문 전에 계산 한 번 더 했다고 시간초과가 떴다
     - 유클리드 호제법
       const euclidean = (a, b) => { if (b === 0) return a; return euclidean(b, a % b); };

---
+++ BOJ/euclidean/2981.js
@@ -0,0 +1,50 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
+
+const dif = [];
+const sortedNumber = input.slice(1).sort((a, b) => a - b);
+sortedNumber.forEach((v, i) => {
+  if (i > 0) {
+    dif.push(v - sortedNumber[i - 1]);
+  }
+});
+
+const euclidean = (a, b) => {
+  while (b !== 0) [a, b] = [b, a % b];
+  return a;
+};
+
+let gcd = dif[0];
+for (let i = 1; i < dif.length; i++) {
+  gcd = euclidean(gcd, dif[i]);
+}
+
+// const getDivisors = (n) => {
+//   const front = [];
+//   const mid = Math.sqrt(n);
+//   for (let i = 1; i < mid; i++) if (n % i === 0) front.push(i);
+//   const back = front.map((v) => n / v).reverse();
+//   if (n % mid === 0) return [...front, mid, ...back];
+//   else return [...front, ...back];
+// };
+
+const getDivisors = (n) => {
+  const divisors = [];
+  for (let i = 1; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      divisors.push(i);
+      if (i !== n / i) divisors.push(n / i);
+    }
+  }
+  return divisors.sort((a, b) => a - b);
+};
+
+const divisors = getDivisors(gcd);
+const remove1 = divisors[0] === 1 ? divisors.slice(1) : divisors;
+const answer = remove1.join(" ");
+
+console.log(answer);
